1d881dd3c3411c431df7df7436ae23932f84a792
sushen123 <sushensame@gmail.com>
2024-01-10 16:59:48 +0545

    finally syling and scripting completed in javascript

---
+++ index.js
@@ -0,0 +1,66 @@
+
+function appendToDisplay(value) {
+    const display1 = document.querySelector(".display1");
+    
+    if(display1.textContent === '0') {
+        display1.textContent = value;
+    }
+    else {display1.textContent += value;
+    }
+}
+
+let previousResults = [];
+function calculate() {
+    const display1 = document.querySelector(".display1");
+    const display2 = document.querySelector(".display2");
+    const expression = display1.textContent;
+  
+   
+    const operators = ['+', '-', '*', '/'];
+    let result = 0;
+  
+    operators.forEach((operator) => {
+      const operands = expression.split(operator);
+  
+      if (operands.length > 1) {
+   
+        switch (operator) {
+          case '+':
+            result = operands.reduce((acc, operand) => acc + parseFloat(operand), 0);
+            break;
+          case '-':
+            result = operands.reduce((acc, operand) => acc - parseFloat(operand), 0);
+            break;
+          case '*':
+            result = operands.reduce((acc, operand) => acc * parseFloat(operand), 1);
+            break;
+          case '/':
+
+            result = operands.reduce((acc, operand) => {
+                const num = parseFloat(operand);
+                return acc !== null && !isNaN(num) && num!== 0 ? acc / num : null;
+             },1);
+             
+             if (result === null) {
+                display1.textContent = 'Error';
+                display2.textContent = 'Error';
+                return;
+              }
+              break;
+        }
+      }
+    });
+  
+    display1.textContent = result;
+    display2.textContent = result
+  }
+
+function clearDisplay() {
+    const display = document.querySelector(".display1");
+    display.textContent = '0';
+
+}
+function deleteLastCharacter() {
+    const display = document.querySelector(".display1");
+    display.textContent = display.textContent.slice(0, -1);
+}
